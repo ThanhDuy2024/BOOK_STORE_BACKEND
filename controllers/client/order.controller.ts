30ecc9f601e5f95b5d1fb7cb88afef5b0b5de9ac
Lam Thanh Duy <thanhduythptthanhbinh@gmail.com>
2026-08-15 13:39:03 +0700

    update: update quantity book when customer order success and check book existe in database

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,9 +1,36 @@
 import { Request, Response } from "express";
 import { Orders } from "../../models/order.model";
 import { Orders_items } from "../../models/orders_items.model";
+import { Books } from "../../models/books.model";
+import { Op, literal } from "sequelize";
 
 export const PostOrderClientController = async (req: Request, res: Response) => {
     try {
+        let redFlag: boolean = true;
+        for (const item of req.body.items) {
+            const checkBook = await Books.findOne({
+                where: {
+                    id: item.id,
+                    status: "active",
+                    quantity: {
+                        [Op.gte]: item.buyQuantity
+                    }
+                }
+            });
+
+            if (!checkBook) {
+                redFlag = false;
+                break;
+            };
+        }
+
+        if (redFlag === false) {
+            return res.status(404).json({
+                status: false,
+                msg: "Number of books exceeds the limit"
+            })
+        }
+
         const order = await Orders.create({
             fullName: req.body.customer.fullName,
             email: req.body.customer.email,
@@ -22,7 +49,13 @@ export const PostOrderClientController = async (req: Request, res: Response) => 
                 price: item.price,
                 orderId: order.dataValues.id
             })
+
+            await Books.update(
+                { quantity: literal(`quantity - ${item.buyQuantity}`) },
+                { where: { id: item.id } }
+            )
         }
+
         res.status(200).json({
             status: true,
             msg: "Order successful!"
